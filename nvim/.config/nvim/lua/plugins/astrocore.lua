96a855f736f6ae47f2ee0f29ed38855472d08817
Anson Lee <leehosanganson@gmail.com>
2026-02-01 20:45:02 +0000

    vim: set autoread
    so it refreshes when something else changed the file externally

--- nvim/.config/nvim/lua/plugins/astrocore.lua
+++ nvim/.config/nvim/lua/plugins/astrocore.lua
@@ -31,6 +31,7 @@ return {
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
         scrolloff = 8, -- sets vim.opt.scrolloff
+        autoread = true, -- sets vim.opt.autoread
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
