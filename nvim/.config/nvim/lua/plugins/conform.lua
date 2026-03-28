3d0d366333ab6163fe2955be9e2fe11100bb0c11
Anson Lee <leehosanganson@gmail.com>
2026-03-28 14:02:48 +0000

    nvim: add yaml and json formatter

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,28 +1,39 @@
 local is_nixos = vim.fn.executable("nixos-rebuild") == 1
+if not is_nixos then return {} end
 
 return {
     "stevearc/conform.nvim",
     opts = function(_, opts)
         opts.formatters_by_ft = opts.formatters_by_ft or {}
+
+        -- nix
         opts.formatters_by_ft = {
             -- Map the filetype to the executable name
             nix = { "statix", "nixpkgs_fmt" },
+            yaml = { "prettierd" },
+            yml = { "prettierd" },
+            json = { "prettierd" },
+            jsonc = { "prettierd" },
         }
-        if is_nixos then
-            opts.formatters = {
-                -- Tell conform how to talk to the 'statix' command you installed via NixOS
-                statix = {
-                    command = "statix",
-                    args = { "fix", "--stdin" },
-                    stdin = true,
-                },
-                nixpkgs_fmt = {
-                    command = "nixpkgs-fmt",
-                },
-            }
-        end
 
-        opts.format_on_save = {
+        opts.formatters       = {
+            -- Tell conform how to talk to the 'statix' command you installed via NixOS
+            statix = {
+                command = "statix",
+                args = { "fix", "--stdin" },
+                stdin = true,
+            },
+            nixpkgs_fmt = {
+                command = "nixpkgs-fmt",
+            },
+            prettierd = {
+                command = "prettierd",
+                args = { "$FILENAME" },
+                stdin = true,
+            },
+        }
+
+        opts.format_on_save   = {
             lsp_fallback = true,
             timeout_ms = 500,
         }
