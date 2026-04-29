d0fce013f397cbcf0c1a8ac9b808a2452e76d83a
Anson Lee <leehosanganson@gmail.com>
2026-04-29 19:40:46 +0100

    nvim: telescope command for fzf

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -17,24 +17,23 @@ return {
     },
     opts = {
       defaults = { file_ignore_patterns = { "node_modules", ".git/" } },
+      pickers = {
+        find_files = {
+          find_command = { "rg", "--files", "--hidden", "--follow", "--no-ignore-vcs" },
+        },
+      },
     },
     config = function(_, opts)
       local function get_project_root()
         local dir = vim.fn.expand "%:p:h"
-        if dir == "" then
-          return vim.fn.getcwd()
-        end
+        if dir == "" then return vim.fn.getcwd() end
         local markers = { ".git", "package.json", "Cargo.toml", "pyproject.toml", "go.mod", ".hg" }
         while true do
           for _, marker in ipairs(markers) do
-            if vim.uv.fs_stat(dir .. "/" .. marker) then
-              return dir
-            end
+            if vim.uv.fs_stat(dir .. "/" .. marker) then return dir end
           end
           local parent = vim.fn.fnamemodify(dir, ":h")
-          if parent == dir then
-            break
-          end
+          if parent == dir then break end
           dir = parent
         end
         return vim.fn.getcwd()
@@ -44,12 +43,18 @@ return {
       telescope.setup(opts)
       pcall(telescope.load_extension, "fzf")
 
-      vim.keymap.set("n", "<leader>ff", function()
-        require("telescope.builtin").find_files { cwd = get_project_root() }
-      end, { desc = "Find files" })
-      vim.keymap.set("n", "<leader>fw", function()
-        require("telescope.builtin").grep_string { cwd = get_project_root() }
-      end, { desc = "Grep word under cursor" })
+      vim.keymap.set(
+        "n",
+        "<leader>ff",
+        function() require("telescope.builtin").find_files { cwd = get_project_root() } end,
+        { desc = "Find files" }
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>fw",
+        function() require("telescope.builtin").grep_string { cwd = get_project_root() } end,
+        { desc = "Grep word under cursor" }
+      )
     end,
   },
 }
