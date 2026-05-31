e0aab85e9461ce05414bb69389dbe76acd654cbc
Anson Lee <leehosanganson@gmail.com>
2026-05-31 15:57:22 +0100

    nvim: treesitter

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -3,15 +3,13 @@ return {
     "nvim-treesitter/nvim-treesitter",
     event = { "BufReadPost", "BufNewFile" },
     build = ":TSUpdate",
+    main = "nvim-treesitter.configs",
     opts = {
       auto_install = false,
       ensure_installed = { "go", "gomod", "gosum", "typescript", "tsx", "javascript" },
       highlight = { enable = true },
       indent = { enable = true },
     },
-    config = function(_, opts)
-      require("nvim-treesitter").setup(opts)
-    end,
   },
   {
     "lukas-reineke/indent-blankline.nvim",
