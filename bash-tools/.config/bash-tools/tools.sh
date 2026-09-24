8373cfc9bbf13c9dd5a7037680d6d4437ab40fa8
Anson Lee <leehosanganson@gmail.com>
2026-09-24 21:36:00 +0100

    tools: remove factory

--- bash-tools/.config/bash-tools/tools.sh
+++ bash-tools/.config/bash-tools/tools.sh
@@ -2,16 +2,6 @@
 
 bash_tools_dir=$HOME/.config/bash-tools
 
-# Factory
-factory_bin=$bash_tools_dir/factory/bin
-case ":$PATH:" in
-  *":$factory_bin:"*) ;;
-  *)
-    if [ -n "$PATH" ]; then PATH=$PATH:$factory_bin; else PATH=$factory_bin; fi
-    ;;
-esac
-export PATH
-
 # Register scripts here
 . "$bash_tools_dir/alias.sh"
 . "$bash_tools_dir/gr.sh"
